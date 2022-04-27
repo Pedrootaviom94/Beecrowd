8a999fe3660d48f22bb0dff80c607ce0d921aff5
Pedrootaviom94 <72772346+Pedrootaviom94@users.noreply.github.com>
2022-04-27 19:43:35 -0300

    atividades do beecrowd

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Beecrowd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {  // double pi = 3.14159;
+            //double raio  = double.Parse(Console.ReadLine());
+            //double area = raio * raio * pi;
+          int A;
+          int B;
+         // Console.WriteLine("SOMA = ");
+           A = int.Parse(Console.ReadLine());
+           B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("PROD = {0}", A * B);
+            
+        }
+    }
+} 
