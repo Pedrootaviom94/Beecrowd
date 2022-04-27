425a431efd66135b33098e68b715976b210116f9
Pedrootaviom94 <72772346+Pedrootaviom94@users.noreply.github.com>
2022-04-27 20:12:48 -0300

    atividade 1002

---
+++ 1002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double pi = 3.14159;
+            double raio  = double.Parse(Console.ReadLine());
+            double area = raio * raio * pi;
+            Console.WriteLine("A={0}",area.ToString("F4"));
+        }
+    }
+}
