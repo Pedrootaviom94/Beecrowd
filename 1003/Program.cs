41f760d7e0deaad078e71f0337f69a305d012f6e
Pedrootaviom94 <72772346+Pedrootaviom94@users.noreply.github.com>
2022-04-27 20:14:16 -0300

    atividade 1003

---
+++ 1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              int A;
+          int B;
+         // Console.WriteLine("SOMA = ");
+           A = int.Parse(Console.ReadLine());
+           B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("SOMA = {0}", A + B);
+        }
+    }
+}
