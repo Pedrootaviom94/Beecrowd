12ae3115dcc8b60a7b5f0015271ace95d3859999
Pedrootaviom94 <72772346+Pedrootaviom94@users.noreply.github.com>
2022-04-27 20:11:03 -0300

    atividade 1001

---
+++ 1001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+  // Console.WriteLine("informe o valor de A");
+  int A = int.Parse(Console.ReadLine());
+   
+  // Console.Wr(iteLine("informe o valor de B");
+ int   B= int.Parse(Console.ReadLine());
+   
+   Console.WriteLine("X = {0}", A+B);
+ 
+   }
+     }
+        }
+    
