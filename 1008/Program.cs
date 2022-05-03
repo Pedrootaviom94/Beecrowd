a741458726491ea370859cebf11321dc5947a422
Pedrootaviom94 <72772346+Pedrootaviom94@users.noreply.github.com>
2022-05-03 20:09:45 -0300

    atividade 1008

---
+++ 1008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+			//Obter dados do usuário
+			double a = double.Parse(Console.ReadLine());
+			double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+			//Printar o resultado
+            Console.WriteLine("NUMBER = {0} ", HorasTrabalhadas(a));
+			Console.WriteLine("SALARY = U$ {0}", CalculoProduto(b, c));
+           
+		}
+         static double HorasTrabalhadas (double a){
+             return (a);
+         }
+
+		static double CalculoProduto(double b, double c) {
+			return (b * c);
+        }
+        
+    }
+}
